280ca95d4df260111908a8411f880eaeb9b88ac2
simleo <simleo@crs4.it>
2017-09-14 17:29:25 +0200

    env setup

--- crs4.hadoop/molecule/default/tests/test_default.py
+++ crs4.hadoop/molecule/default/tests/test_default.py
@@ -6,9 +6,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+ROLES_DIR = os.path.join(THIS_DIR, *[os.pardir]*3)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_hadoop_cmd(host):
+    defaults = host.ansible(
+        "include_vars",
+        os.path.join(ROLES_DIR, "defaults", "main.yml")
+    )["ansible_facts"]
+    hadoop = os.path.join(
+        defaults["hadoop_parent"], "hadoop", "bin", "hadoop"
+    )
+    out = host.check_output("%s version" % hadoop)
+    hadoop_version = out.split("\n", 1)[0].split(" ")[-1]
+    assert hadoop_version == defaults["hadoop_version"]
