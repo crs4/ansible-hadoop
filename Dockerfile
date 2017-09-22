3525991b6fdcabffcaae8ee027ff5984c443ea83
simleo <simleo@crs4.it>
2017-09-22 18:11:57 +0200

    configurable log dir; allow hdfs access from outside the container

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,12 @@ COPY playbook.yml requirements.yml /build/
 RUN ansible-galaxy install -r /build/requirements.yml && \
     ansible-playbook /build/playbook.yml
 
+# disable IPv6
+RUN echo "net.ipv6.conf.all.disable_ipv6 = 1" >>/etc/sysctl.conf && \
+    echo "net.ipv6.conf.default.disable_ipv6 = 1" >>/etc/sysctl.conf
+
 COPY entrypoint.sh /
 
 EXPOSE 8020 8042 8088 9000 10020 19888 50010 50020 50070 50075 50090
 
-CMD "/entrypoint.sh"
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,12 @@ function onshutdown {
 trap onshutdown SIGTERM
 trap onshutdown SIGINT
 
+# allow HDFS access from outside the container
+sed -i s/localhost/${HOSTNAME}/ /opt/hadoop/etc/hadoop/core-site.xml
+
+if [ $# -gt 0 ]; then
+    [ $1 == "-f" ] && /opt/hadoop/bin/hadoop namenode -format -force
+fi
 /opt/hadoop/sbin/hadoop-daemon.sh start namenode
 /opt/hadoop/sbin/hadoop-daemon.sh start datanode
 /opt/hadoop/bin/hdfs dfsadmin -safemode wait
@@ -21,6 +27,6 @@ trap onshutdown SIGINT
 /opt/hadoop/sbin/yarn-daemon.sh start nodemanager
 /opt/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
 
-tail -f /opt/hadoop/logs/*
+tail -f /dev/null
 
 onshutdown
