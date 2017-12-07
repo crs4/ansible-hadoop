535e055ad5e7a4874f1be90441e6585d243298ea
simleo <simleo@crs4.it>
2017-12-07 12:06:14 +0100

    entrypoint.sh: always format HDFS; ad timeout to safemode wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,11 @@ trap onshutdown SIGINT
 # allow HDFS access from outside the container
 sed -i s/localhost/${HOSTNAME}/ /opt/hadoop/etc/hadoop/core-site.xml
 
-if [ $# -gt 0 ]; then
-    [ $1 == "-f" ] && /opt/hadoop/bin/hadoop namenode -format -force
-fi
+/opt/hadoop/bin/hadoop namenode -format -force
 /opt/hadoop/sbin/hadoop-daemon.sh start namenode
 /opt/hadoop/sbin/hadoop-daemon.sh start datanode
-/opt/hadoop/bin/hdfs dfsadmin -safemode wait
+timeout 3 bash -c -- '/opt/hadoop/bin/hdfs dfsadmin -safemode wait' || \
+    /opt/hadoop/bin/hdfs dfsadmin -safemode leave
 /opt/hadoop/sbin/yarn-daemon.sh start resourcemanager
 /opt/hadoop/sbin/yarn-daemon.sh start nodemanager
 /opt/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
