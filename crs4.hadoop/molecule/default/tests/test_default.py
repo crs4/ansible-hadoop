3c3087d9824bcabe8eed905a0482fb494cbf8984
simleo <simleo@crs4.it>
2018-04-12 15:06:25 +0200

    updated tests

--- crs4.hadoop/molecule/default/tests/test_default.py
+++ crs4.hadoop/molecule/default/tests/test_default.py
@@ -11,13 +11,17 @@ ROLES_DIR = os.path.join(THIS_DIR, *[os.pardir]*3)
 
 
 def test_hadoop_cmd(host):
+    variables = host.ansible.get_variables()
     defaults = host.ansible(
         "include_vars",
         os.path.join(ROLES_DIR, "defaults", "main.yml")
     )["ansible_facts"]
+    exp_hadoop_version = variables.get(
+        "hadoop_version", defaults["hadoop_version"]
+    )
     hadoop = os.path.join(
         defaults["hadoop_parent"], "hadoop", "bin", "hadoop"
     )
     out = host.check_output("%s version" % hadoop)
     hadoop_version = out.split("\n", 1)[0].split(" ")[-1]
-    assert hadoop_version == defaults["hadoop_version"]
+    assert hadoop_version == exp_hadoop_version
